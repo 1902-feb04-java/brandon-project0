18b7e32fefd84f018b4c41a17328b86812985b1f
Brandon <tubocass@gmail.com>
2019-02-18 21:14:42 -0600

    seperated board and cell size variables. Resized board to 80x60 with 8px cells

--- src/life.js
+++ src/life.js
@@ -11,16 +11,19 @@
 'use strict';
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
-const width = 32, height = 32;
+const boardWidth = 80, boardHeight = 60;
+const cellWidth = 8, cellHeight = 8;
+
 canvas.addEventListener('click',(e)=>{
     let rect = canvas.getBoundingClientRect();
     let mouseLoc = {
-        x : Math.floor((e.clientX - rect.left)/width),
-        y : Math.floor((e.clientY - rect.top)/height)
+        x : Math.floor((e.clientX - rect.left)/cellWidth),
+        y : Math.floor((e.clientY - rect.top)/cellHeight)
     }
     console.log(mouseLoc);
     // convert from canvas - window
-    getCellByLocation(mouseLoc).nextState = true;
+    let c = getCellByLocation(mouseLoc);
+    if(c != null)c.nextState = true;
 })
 function Cell(xPos, yPos, alive)
 {
@@ -33,6 +36,37 @@ function Cell(xPos, yPos, alive)
     }
 }
 
+const board = createBoard();
+
+function createBoard()
+{
+    let newBoard = [[],[]];
+    for (let x = 0; x < boardWidth; x++)
+    {
+        newBoard[x] =[];
+        for(let y = 0; y < boardHeight; y++)
+        {
+            let xPosition = x*cellWidth;
+            let yPosition = y*cellHeight;
+            let alive = Math.random() > 0.8;
+            let c = new Cell(xPosition, yPosition, alive);
+            newBoard[x][y] = c;
+            drawSquare(xPosition, yPosition, c.isAlive);
+            // console.log(newBoard[x][y]);
+        }
+    }
+    return newBoard;
+}
+
+function drawSquare(x,y, bool)
+{
+    ctx.beginPath();
+    ctx.rect(x, y, cellWidth, cellHeight)
+    ctx.fillStyle = bool? 'white': 'red';
+    ctx.fill();
+    ctx.closePath();
+}
+
 function checkNeighbors(cell, grid)
 {
     // let arr = [];//for debugging
@@ -42,15 +76,17 @@ function checkNeighbors(cell, grid)
     // let negDist = -radius;
     let negRow = -radius;
     let negCol;
+    let row; 
+    let col;
     for(let x = 0; x<dist; x++)
     {
         negCol = -radius; //reset for each row
         for(let y = 0; y<dist; y++)
         {
-            let row = negRow + cell.x/width; 
-            let col = negCol + cell.y/height;
+            row = negRow + cell.x/cellWidth; 
+            col = negCol + cell.y/cellHeight;
 
-            if(row<width && row >=0 && col<height && col>=0)//only adding valid board locations
+            if(row<boardWidth && row >=0 && col<boardHeight && col>=0)//only adding valid board locations
             {
             // debugger;
 
@@ -76,38 +112,13 @@ function getCellByLocation(vector)
 {
     let x = Math.round(vector.x);//we use a unit size of 1, elsewise multiply by size
     let y = Math.round(vector.y);
+
+    if(x<0||x>=boardWidth||y<0||y>boardHeight)
+    return null;
+
     if(board!=null && board[x][y]!=null)
     return board[x][y];
     else return null;
-}
-
-const board = createBoard();
-
-function createBoard()
-{
-    let newBoard = [[],[]];
-    for (let x = 0; x < width; x++)
-    {
-        newBoard[x] =[];
-        for(let y = 0; y < height; y++)
-        {
-            let alive = Math.random() > 0.6;
-            let c = new Cell(x*width, y*height, alive);
-            newBoard[x][y] = c;
-            drawSquare(c.x, c.y, c.isAlive);
-            // console.log(newBoard[x][y]);
-        }
-    }
-    return newBoard;
-}
-
-function drawSquare(x,y, bool)
-{
-    ctx.beginPath();
-    ctx.rect(x, y, 32, 32)
-    ctx.fillStyle = bool? 'white': 'red';
-    ctx.fill();
-    ctx.closePath();
 }
 
 function tick()
@@ -117,9 +128,9 @@ function tick()
             first to decide whether each cell lives or dies
             second to change state after evaluation
     */
-    for(let x = 0; x < width; x++)
+    for(let x = 0; x < boardWidth; x++)
     {
-        for(let y = 0; y < height; y++)
+        for(let y = 0; y < boardHeight; y++)
         {
             let cell = board[x][y];
             let neighbors = checkNeighbors(cell, board); //how many neighbors are 'Alive'
@@ -129,7 +140,7 @@ function tick()
                 {
                     case 0: // less than 2 neighbors leads to death due to underpopulation
                     case 1:  cell.nextState = false; break;
-                    case 2: //2 or 3 neighbors keeps the cell alive
+                    case 2: //2 or 3 neighbors keeps the cell alive - stable population
                     case 3:  cell.nextState = true; break;
                     case 4: // more than 3 neighbors leads to death due
                     case 5: // to overpopulation
@@ -145,9 +156,9 @@ function tick()
             }
         }
     }
-    for(let x = 0; x < width; x++)
+    for(let x = 0; x < boardWidth; x++)
     {
-        for(let y = 0; y < height; y++)
+        for(let y = 0; y < boardHeight; y++)
         {
             let cell = board[x][y];
             cell.change();
